cd127f8b8a3f81a4b43eb74df1aacfb9305eccef
Roll Offle <rolloffle@gmail.com>
2012-11-02 01:25:12 +0000

    Write timestamp for when begging started as well

--- collate.py
+++ collate.py
@@ -14,18 +14,27 @@ for directory, subdirs, filenames in os.walk("projects/"):
 # now open each individual file and add it to a master data file R can read
 
 output = open("infos.txt", "wb")
-output.write("id\ttimestamp\tt\tchumps\tgoal\tfunds\tf\n") # want to be able to name columns with better names than R's default "V2" shit
+output.write("id\tstart\tnow\tt\tchumps\tgoal\tfunds\tf\n") # set better column names than R's default "V2" shit
 
+empties = 0
 for i in xrange(len(projects)):
 	project = projects[i]
 	everything = open(project).readlines()
+	if everything == []:
+		# project data file is empty, no biggie, just count it then skip it
+		empties += 1
+		continue
 	summary = everything[0].split("\t")
 	data = everything[1:]
+	start = summary[3][:-6] # cut off useless timezone that's always +0000
 	goal = float(summary[5])
 	#print i, project, summary, goal
 	for point in data:
 		timestamp, normedtime, chumps, funds = point.strip().split("\t")
+		timestamp = timestamp[:-6] # cut off that useless timezone again
 		funds = float(funds)
-		output.write("%i\t%s\t%.6f\t%s\t%i\t%i\t%.6f\n" % (i+1, timestamp, float(normedtime), chumps, goal, funds, funds / goal))
+		output.write("%i\t%s\t%s\t%.6f\t%s\t%i\t%i\t%.6f\n" % (i+1, start, timestamp, float(normedtime), chumps, goal, funds, funds / goal))
 
 output.close()
+
+print empties, "projects have no individual data file yet btw"
